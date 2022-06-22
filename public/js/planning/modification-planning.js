d315f653780616572a6c6086c9a0c6c9c2337e0d
Vincent.blanco <vincent.blanco@etu.univ-tours.fr>
2022-06-22 14:27:04 +0200

    planning : évolution des modales

--- public/js/planning/modification-planning.js
+++ public/js/planning/modification-planning.js
@@ -1,34 +1,45 @@
-//var date = document.getElementById(date).value;
-//alert(document.getElementById(date).value);
-var calendar;
 document.addEventListener('DOMContentLoaded', function() 
 {
     const height = document.querySelector('div').clientHeight;
     var calendarEl = document.getElementById('calendar');
-    calendar = new FullCalendar.Calendar(calendarEl, 
+
+    //Créer le calendar sous les conditions que l'on souhaite
+    var calendar = new FullCalendar.Calendar(calendarEl, 
     {
+        //clé de la license pour utiliser la librairie à des fin non commerciale
         schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+
+        //initialise la vue en colonne par ressource par jour en horaire française
         initialView: 'resourceTimelineDay',
         slotDuration: '00:20:00',
         locale: 'fr',
         timeZone: 'Europe/Paris',
+
+        //permet de modifier les events dans le calendar
         selectable: true,
         selectHelper: true,
         editable: true,
-        contentHeight: height-120,
-        handleWindowResize: true,
         eventDurationEditable: false,
+
+        contentHeight: 9/12*height,
+        handleWindowResize: true,
+
+        //modifie l'affichage de l'entête du calendar pour ne laisser que la date du jour
         headerToolbar: {
             start: null,
             center: 'title',
             end: null
         },
-        slotLabelFormat: { //modifie l'affichage des heures de la journée
-            hour: '2-digit', //2-digit, numeric
-            minute: '2-digit', //2-digit, numeric
-            meridiem: false, //lowercase, short, narrow, false (display of AM/PM)
-            hour12: false //true, false
-          },
+
+        //modifie l'affichage des heures de la journée
+        slotLabelFormat: { 
+            hour: '2-digit',
+            minute: '2-digit',
+            meridiem: false,
+            hour12: false
+        },
+
+        //à supprimer
         resources: [
         {
             id: 'a',
@@ -50,37 +61,47 @@ document.addEventListener('DOMContentLoaded', function()
              textColor:'#000',
              textFont:'Trebuchet MS'
             }
-            ],
-            eventClick: function(event, element) {
-                var id = event.event._def.publicId;
-                var activity = calendar.getEventById(id);
-                var tmp = activity.end - activity.start;
-                time = Math.floor((tmp/1000/60));
+        ],
+        //à supprimer
+
+        //permet d'ouvrir la modal pour la modification d'une activité lorsque l'on click dessus
+        eventClick: function(event, element) {
+            //récupération des données
+            var id = event.event._def.publicId;
+            var activity = calendar.getEventById(id);
+            var start = activity.start;
+            var tmp = activity.end - start;
+
+            //calcul de la durée de l'activité
+            length = Math.floor((tmp/1000/60));
+            const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+            date = start.toLocaleDateString(undefined, options);
     
-                const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-                date = activity.start.toLocaleDateString(undefined, options);
-    
-                $('#new-start').val(date);
-                $('#new-title').val(activity.title);
-                $('#new-time').val(time);
-                $('#modify-planning-modal').modal("show");
-            },
+            //set les données à afficher par défault
+            $('#new-start').val(date);
+            $('#new-title').val(activity.title);
+            $('#length').val(length);
+            $('#id').val(id);
+
+            //ouvre la modal
+            $('#modify-planning-modal').modal("show");
+        },
     },
     );
-    //alert(date);
-    //calendar.goToDate(date);
+
+    //affiche le calendar
     calendar.render();
 });
 
+//function permettant la modification de l'activité
 function modifyEvent(){
-    
     document.getElementById("succ").innerHTML="Modification de l'activité réussie";
     document.getElementById("success").style.display="block";
     unshowDiv('modify-planning-modal');
     setTimeout(()=>{document.getElementById("success").style.display="none";},6000);
-    
 }
 
+//function permettant l'ouverture de la modal d'ajout d'un parcours
 function addEvent(){
     $('#add-planning-modal').modal("show");
 }
--- src/Controller/ModificationPlanningController.php
+++ src/Controller/ModificationPlanningController.php
@@ -4,15 +4,44 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * Controller de la page modification du planning
+ */
 class ModificationPlanningController extends AbstractController
 {
+    /**
+     * Fonction pour l'affichage de la page modification planning par la méthode GET
+     */
     public function modificationPlanningGet(ManagerRegistry $doctrine): Response
     {
+        //Récupération des données ressources de la base de données
         $repository = $doctrine->getRepository("App\Entity\ResourceType");
         $resourcestypes = $repository->findAll();
 
         return $this->render('planning/modification-planning.html.twig', ['resourcestypes' => $resourcestypes ]);
+    }
+
+    public function modificationPlanningPost(Request $request, ManagerRegistry $doctrine)
+    {
+        $form = $request->request->get('form');
+        $title = $request->request->get('title');
+        $start = $request->request->get('start');
+        $length = $request->request->get('length');
+
+        if($form == 'modify')
+        {
+            $id = $request->request->get('id');
+            echo "</br>" . "je modifie" . "</br>";
+
+        }
+        else if($form == 'add')
+        {
+            echo "</br>" . "j'ajoute" . "</br>";
+        }
+
+        //dd($request);
     }
 }
