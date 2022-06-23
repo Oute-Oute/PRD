55aebf4fc238abe34550d8c2ce7b6ab84129b079
Vincent.blanco <vincent.blanco@etu.univ-tours.fr>
2022-06-23 14:19:15 +0200

    bdd : correction

--- src/Entity/ResourceType.php
+++ src/Entity/ResourceType.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Repository\ResourceTypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -46,7 +47,6 @@ class ResourceType
     public function __construct()
     {
         $this->resources = new ArrayCollection();
-        $this->activities = new ArrayCollection();
     }
 
     public function getId(): ?int
