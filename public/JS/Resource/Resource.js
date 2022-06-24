e6a27dee13082c3273a3da21ecbe637967e275d8
Virgile <75625115+VirgileVV1@users.noreply.github.com>
2022-06-24 16:22:00 +0200

    FRONT/BACK Create ResourceType with modal form

--- public/JS/Resource/Resource.js
+++ public/JS/Resource/Resource.js
@@ -1,37 +1,29 @@
 function showNewModalForm() {
     containerModal = document.getElementById('container-modal')
     containerModal.style.display = "flex";
-    console.log("container flex")
-
+    
     formAddActivity = document.getElementById('form-add-activity');
-    formAddActivity.style.display = "flex";
-    console.log("act flex")
+    formAddActivity.style.display = "none";
 
     formAddResource = document.getElementById('form-add-resource');
-    formAddResource.style.display = "none";
-    console.log("res none")
+    formAddResource.style.display = "flex";
 
     formAddResourcetype = document.getElementById('form-add-resourcetype');
     formAddResourcetype.style.display = "none";
-    console.log("restype none")
 }
 
 function hideNewModalForm() {
     containerModal = document.getElementById('container-modal')
     containerModal.style.display = "none";
-    console.log("container none")
 
     formAddActivity = document.getElementById('form-add-activity');
     formAddActivity.style.display = "none";
-    console.log("act none")
 
     formAddResource = document.getElementById('form-add-resource');
     formAddResource.style.display = "none";
-    console.log("res none")
 
     formAddResourcetype = document.getElementById('form-add-resourcetype');
     formAddResourcetype.style.display = "none";
-    console.log("restype none")
 }
 
 
