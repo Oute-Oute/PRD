31f21fdf0b52f46d2d524728504901822ad65cfc
Vincent.blanco <vincent.blanco@etu.univ-tours.fr>
2022-06-28 15:13:14 +0200

    patient : correction des modales add et del

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -20,8 +20,7 @@ class PatientController extends AbstractController
     public function patientGet(PatientRepository $patientRepository, ManagerRegistry $doctrine): Response
     {
         return $this->render('patient/index.html.twig', [
-            'patients' => $patientRepository->findAll(),
-            'pathways' => $doctrine->getRepository("App\Entity\Pathway")->findAll()
+            'patients' => $patientRepository->findAll()
         ]);
     }
 
@@ -29,20 +28,19 @@ class PatientController extends AbstractController
     {
         // Méthode POST pour ajouter un circuit
         if ($request->getMethod() === 'POST' ) {
-    
             // On recupere toutes les données de la requete
             $param = $request->request->all();
 
-            $lastname = $param['lastname'];             // le nom
+            $lastname = $param['lastname'];       // le nom
             $firstname = $param['firstname'];     // le prenom
 
-            // Création de l'activité
+            // Création du patient
             $patient = new Patient(); 
             $patient->setLastname($lastname);
             $patient->setFirstname($firstname);
-          
-            // ajout dans la bd 
-            $patientRepository->add($patient, false);
+            
+            // ajout dans la bdd
+            $patientRepository->add($patient, true);
 
             return $this->redirectToRoute('Patients', [], Response::HTTP_SEE_OTHER);
         }
@@ -67,27 +65,49 @@ class PatientController extends AbstractController
 
     public function patientDelete(Patient $patient, PatientRepository $patientRepository): Response
     {
+        //suppression des données associées au patient de la table Appointment
         $appointmentRepository = $this->getDoctrine()->getManager()->getRepository("App\Entity\Appointment");
-        $appointments = $appointmentRepository->findAll();
+        $appointments = $appointmentRepository->findBy(['patient_id' => $patient->getId()]);
+
         foreach($appointments as $appointment)
         {
-            if($appointment->getPatient() == $patient)
-            {
-                $appointmentRepository->remove($appointment, true);
-            }
+            $appointmentRepository->remove($appointment, true);
         }
 
-        $appointmentRepository = $this->getDoctrine()->getManager()->getRepository("App\Entity\ScheduledActivity");
-        $appointments = $appointmentRepository->findAll();
-        foreach($appointments as $appointment)
+
+        //suppression des données associées au patient dans les tables ScheduledActivity, MaterialResourceScheduled et HumanResourceScheduled 
+        $scheduledActivityRepository = $this->getDoctrine()->getManager()->getRepository("App\Entity\ScheduledActivity");
+        $scheduledActivities = $scheduledActivityRepository->findBy(['patient_id' => $patient->getId()]);
+
+        foreach($scheduledActivities as $scheduledActivity)
         {
-            if($appointment->getPatient() == $patient)
+            //suppression des données associées au patient de la table MaterialResourceScheduled
+            $materialResourceScheduledRepository = $this->getDoctrine()->getManager()->getRepository("App\Entity\MaterialResourceScheduled");
+            $allMaterialResourceScheduled = $materialResourceScheduledRepository->findBy(['scheduledactivity_id' => $scheduledActivity->getId()]);
+
+            foreach($allMaterialResourceScheduled as $materialResourceScheduled)
             {
-                $appointmentRepository->remove($appointment, true);
+                $materialResourceScheduledRepository->remove($materialResourceScheduled, true);
             }
+
+
+            //suppression des données associées au patient de la table HumanResourceScheduled
+            $humanResourceScheduledRepository = $this->getDoctrine()->getManager()->getRepository("App\Entity\HumanResourceScheduled");
+            $allHumanResourceScheduled = $humanResourceScheduledRepository->findBy(['scheduledactivity_id' => $scheduledActivity->getId()]);
+
+            foreach($allHumanResourceScheduled as $humanResourceScheduled)
+            {
+                $humanResourceScheduledRepository->remove($humanResourceScheduled, true);
+            }
+
+
+            //suppression des données associées au patient de la table ScheduledActivity
+            $scheduledActivityRepository->remove($scheduledActivity, true);
         }
 
+        //suppression du patient dans la table Patient
         $patientRepository->remove($patient, true);
+
         return $this->redirectToRoute('Patients', [], Response::HTTP_SEE_OTHER);
     }
 }
