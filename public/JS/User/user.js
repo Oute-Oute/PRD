a6e599aeb32e2139f31359974da83c30c52578f7
Fourrep <76952150+Fourrep@users.noreply.github.com>
2022-07-06 11:53:47 +0200

    UserPage (Correction et Amélioration)

--- public/JS/User/user.js
+++ public/JS/User/user.js
@@ -1,12 +1,16 @@
+const { format } = require("core-js/core/date");
+const { random } = require("core-js/core/number");
+
 function addUser(){
     $('#add-user-modal').modal("show");
 }
 
-function editUser(id, username ) {
-    document.getElementById('iduser').value = id;
-    document.getElementById('username').value = username;
-    //document.getElementById('role').value = role;
-    $('#edit-user-modal').modal("show");
+function editUser(idEdit, usernameEdit ) {
+    document.getElementById('iduserEdit').value = idEdit;
+    document.getElementById('usernameEdit').value = usernameEdit;
+      
+    $('#edit-user-modal').modal("show");   
+    
 }
 
 function showPatient(id) {
@@ -14,4 +18,49 @@ function showPatient(id) {
     document.getElementById('username').value = username;
     document.getElementById('role').value = role;
     $('#show-user-modal').modal("show");
+}
+
+function usernameTestEdit(){
+   
+  var listeUser=JSON.parse(document.getElementById("userList").value);
+  let usernamerequest = document.getElementById('username').value;  
+  var id = document.getElementById('iduser').value;  
+  console.log(id)
+  let dispo = true    
+  for (let i = 0; i < listeUser.length; i++){      
+     if (usernamerequest == listeUser[i].username && !(id==listeUser[i].id)){
+      dispo = false;  
+       break   ;
+     }      
+    }      
+    if (!dispo) {
+      document.getElementById('buttonConfirmEdit').disabled = true;
+     document.getElementById('ErrorUserEdit').style.visibility = 'visible';        
+}
+    else {
+      document.getElementById('buttonConfirmEdit').disabled = false;
+      document.getElementById('ErrorUserEdit').style.visibility = 'hidden';
+    }     
+   
+}  
+    function usernameTestNew(){  
+    
+    var listeUser=JSON.parse(document.getElementById("userList").value);
+    let usernamerequest = document.getElementById('usernameAdd').value;   
+    let dispo = true    
+    for (let i = 0; i < listeUser.length; i++){      
+       if (usernamerequest == listeUser[i].username){
+        dispo = false;  
+         break   ;
+       }      
+      }      
+      if (!dispo) {
+        document.getElementById('buttonConfirmAdd').disabled = true;
+       document.getElementById('ErrorUser').style.visibility = 'visible';        
+}
+      else {
+        document.getElementById('buttonConfirmAdd').disabled = false;
+        document.getElementById('ErrorUser').style.visibility = 'hidden';
+      }
+            
 }
