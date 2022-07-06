9fdfb7c90864b65f3254bdfd43789a2da6e22abf
luc.chereau <luc.chereau@etu.univ-tours.fr>
2022-07-06 13:55:54 +0200

    planning : modification : connaitre les ressources allouées d'un event lorsqu'il est drop

--- public/js/planning/modification-planning.js
+++ public/js/planning/modification-planning.js
@@ -1,3 +1,5 @@
+
+
 // Timeout pour afficher le popup (pour éviter une modif trop longue)
 var popupClicked = false;
 var modifAlertTime = 480000; // En millisecondes
@@ -29,7 +31,6 @@ function $_GET(param) {
   return vars;
 }
 
-console.log(dateStr);
 
 document.addEventListener("DOMContentLoaded", function () {
   //Créer le calendar sous les conditions que l'on souhaite
@@ -536,10 +537,11 @@ function createCalendar(typeResource) {
     eventDrop: function (event) {
       var oldEvent = event.oldEvent;
       var modifyEvent = event.event;
-      console.log(oldEvent, modifyEvent)
+      
       var newDelay = oldEvent.start.getTime() - modifyEvent.start.getTime();
       var clickModify = false;
       updateEventsAppointment(oldEvent, newDelay, clickModify);
+      console.log(RessourcesAllocated(modifyEvent)); 
     }
   });
   switch (typeResource) {
@@ -675,4 +677,19 @@ function deleteModifInDB(popupClicked){
   } else {
     window.location.assign("/ModificationDeleteOnUnload?dateModified=" + $_GET('date'));
   }
+}
+
+function RessourcesAllocated(event){
+    
+    if(event._def.resourceIds.includes('m-default')){
+        return 'red';  
+    }
+    else if(event._def.resourceIds.includes('h-default')){
+        return 'red'; 
+    }
+
+    else{
+      return 'green';
+    }
+
 }
