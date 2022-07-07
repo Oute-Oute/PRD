512b0ddfabfab78600bcb3a451fe27bbe082c6f1
Vincent.blanco <vincent.blanco@etu.univ-tours.fr>
2022-07-07 15:58:29 +0200

    modification planning : correction tooltip

--- public/js/planning/modification-planning.js
+++ public/js/planning/modification-planning.js
@@ -687,14 +687,14 @@ function createCalendar(typeResource) {
     resourceAreaWidth: "20%",
     resourceAreaHeaderContent: headerResources,
 
-    eventDidMount: function (info) {
+    /*eventDidMount: function (info) {
       $(info.el).tooltip({
         title: info.event.extendedProps.description,
         placement: "top",
         trigger: "hover",
         container: "body",
       });
-    },
+    },*/
 
     //permet d'ouvrir la modal pour la modification d'une activité lorsque l'on click dessus
     eventClick: function (event) {
