cf1647710aabf4097766355051e946459335ddaa
Vincent.blanco <vincent.blanco@etu.univ-tours.fr>
2022-07-19 16:27:01 +0200

    modification planning : list error messages V2

--- public/js/planning/modification-planning.js
+++ public/js/planning/modification-planning.js
@@ -939,7 +939,7 @@ function createCalendar(typeResource,useCase) {
       var modifyEvent = event.event;
       updateEventsAppointment(oldEvent);
       calendar.render();
-      //updateErrorMessages();
+      updateErrorMessages();
       
       listeHumanResources=JSON.parse(document.getElementById('human').value.replaceAll('3aZt3r',' ')); 
       listeMaterialResources=JSON.parse(document.getElementById('material').value.replaceAll('3aZt3r',' '));
@@ -1168,10 +1168,17 @@ function updateErrorMessages() {
         listErrorMessages.forEach((errorMessage) => {
           if(scheduledActivity._def.extendedProps.appointment == errorMessage.appointmentId){
             appointmentAlreadyExist = true;
+
+            errorMessage.messageEarliestAppointmentTime = getMessageEarliestAppointmentTime(listScheduledActivities, scheduledActivity._def.extendedProps.appointment);
+            errorMessage.messageLatestAppointmentTime = getMessageLatestAppointmentTime(listScheduledActivities, scheduledActivity._def.extendedProps.appointment);
+
             var scheduledActivityAlreadyExist = false;
             errorMessage.listScheduledActivity.forEach((existingScheduledActivity) => {
               if(existingScheduledActivity.scheduledActivityId == scheduledActivity._def.publicId){
                 scheduledActivityAlreadyExist = true;
+                existingScheduledActivity.messageDelay = getMessageDelay(listScheduledActivities, scheduledActivity);
+                existingScheduledActivity.listCategoryHumanResources = getListCategoryHumanResources(scheduledActivity);
+                existingScheduledActivity.listCategoryMaterialResources = getListCategoryMaterialResources(scheduledActivity);
               }
             })
             if(scheduledActivityAlreadyExist == false){
@@ -1281,44 +1288,157 @@ function getMessageDelay(listScheduledActivities, scheduledActivity){
 function getListCategoryHumanResources(scheduledActivity){
   var listCategoryHumanResources = [];
 
+  var listCategoryOfHumanResources = JSON.parse(document.getElementById("categoryOfHumanResourceJSON").value.replaceAll("3aZt3r", " "));
+
+  scheduledActivity._def.resourceIds.forEach((humanResource) => {
+    if(humanResource.substring(0,5) == "human"){
+      listCategoryOfHumanResources.forEach((categoryOfHumanResource) => {
+        if(categoryOfHumanResource.idresource == humanResource){
+          var categoryHumanResourceAlreadyExist = false;
+          if(listCategoryHumanResources != []){
+            listCategoryHumanResources.forEach((categoryHumanResource) => {
+              if(categoryHumanResource.categoryHumanResourceId == categoryOfHumanResource.idcategory){
+                categoryHumanResourceAlreadyExist = true;
+
+                categoryHumanResource.messageCategoryQuantity = getMessageCategoryQuantity(categoryOfHumanResource.idcategory);
+                categoryHumanResource.messageWrongCategory = getMessageWrongCategory(categoryOfHumanResource.idcategory);
+
+                var humanResourceAlreadyExist = false;
+                categoryHumanResource.listHumanResources.forEach((existingHumanResource) => {
+                  if(existingHumanResource.humanResourceId == humanResource){
+                    humanResourceAlreadyExist = true;
+                    existingHumanResource.messageWorkingHours = getMessageWorkingHours(scheduledActivity, humanResource);
+                    existingHumanResource.messageUnavailability = getMessageUnavailability(scheduledActivity, humanResource);
+                    existingHumanResource.messageAlreadyScheduled = getMessageAlreadyExist(scheduledActivity, humanResource);
+                  }
+                })
+                if(humanResourceAlreadyExist == false){
+                  categoryHumanResource.listHumanResources.push({
+                    humanResourceId: humanResource,
+                    humanResourceName: getResourceTitle(humanResource),
+                    messageWorkingHours: getMessageWorkingHours(scheduledActivity, humanResource),
+                    messageUnavailability: getMessageUnavailability(scheduledActivity, humanResource),
+                    messageAlreadyScheduled: getMessageAlreadyExist(scheduledActivity, humanResource)
+                  })
+                }
+              }
+            })
+          }
+          if(categoryHumanResourceAlreadyExist == false){
+            listCategoryHumanResources.push({
+              categoryHumanResourceId: categoryOfHumanResource.idcategory,
+              messageCategoryQuantity: getMessageCategoryQuantity(categoryOfHumanResource.idcategory),
+              messageWrongCategory: getMessageWrongCategory(categoryOfHumanResource.idcategory),
+              listHumanResources: [{
+                humanResourceId: humanResource,
+                humanResourceName: getResourceTitle(humanResource),
+                messageWorkingHours: getMessageWorkingHours(scheduledActivity, humanResource),
+                messageUnavailability: getMessageUnavailability(scheduledActivity, humanResource),
+                messageAlreadyScheduled: getMessageAlreadyExist(scheduledActivity, humanResource)
+              }]
+            })
+          }
+        }
+      })
+    }
+  })
+
   return listCategoryHumanResources;
 }
 
 function getListCategoryMaterialResources(scheduledActivity){
   var listCategoryMaterialResources = [];
 
+  var listCategoryOfMaterialResources = JSON.parse(document.getElementById("categoryOfMaterialResourceJSON").value.replaceAll("3aZt3r", " "));
+
+  scheduledActivity._def.resourceIds.forEach((materialResource) => {
+    if(materialResource.substring(0,8) == "material"){
+      listCategoryOfMaterialResources.forEach((categoryOfMaterialResource) => {
+        if(categoryOfMaterialResource.idresource == materialResource){
+          var categoryMaterialResourceAlreadyExist = false;
+          if(listCategoryMaterialResources != []){
+            listCategoryMaterialResources.forEach((categoryMaterialResource) => {
+              if(categoryMaterialResource.categoryMaterialResourceId == categoryOfMaterialResource.idcategory){
+                categoryMaterialResourceAlreadyExist = true;
+
+                categoryMaterialResource.messageCategoryQuantity = getMessageCategoryQuantity(categoryOfMaterialResource.idcategory);
+                categoryMaterialResource.messageWrongCategory = getMessageWrongCategory(categoryOfMaterialResource.idcategory);
+
+                var materialResourceAlreadyExist = false;
+                categoryMaterialResource.listMaterialResources.forEach((existingMaterialResource) => {
+                  if(existingMaterialResource.humanResourceId == materialResource){
+                    materialResourceAlreadyExist = true;
+                    existingMaterialResource.messageWorkingHours = getMessageWorkingHours(scheduledActivity, materialResource);
+                    existingMaterialResource.messageUnavailability = getMessageUnavailability(scheduledActivity, materialResource);
+                    existingMaterialResource.messageAlreadyScheduled = getMessageAlreadyExist(scheduledActivity, materialResource);
+                  }
+                })
+                if(materialResourceAlreadyExist == false){
+                  categoryMaterialResource.listMaterialResources.push({
+                    materialResourceId: materialResource,
+                    materialResourceName: getResourceTitle(materialResource),
+                    messageUnavailability: getMessageUnavailability(scheduledActivity, materialResource),
+                    messageAlreadyScheduled: getMessageAlreadyExist(scheduledActivity, materialResource)
+                  })
+                }
+              }
+            })
+          }
+          if(categoryMaterialResourceAlreadyExist == false){
+            listCategoryMaterialResources.push({
+              categoryMaterialResourceId: categoryOfMaterialResource.idcategory,
+              messageCategoryQuantity: getMessageCategoryQuantity(categoryOfMaterialResource.idcategory),
+              messageWrongCategory: getMessageWrongCategory(categoryOfMaterialResource.idcategory),
+              listMaterialResources: [{
+                materialResourceId: materialResource,
+                materialResourceName: getResourceTitle(materialResource),
+                messageUnavailability: getMessageUnavailability(scheduledActivity, materialResource),
+                messageAlreadyScheduled: getMessageAlreadyExist(scheduledActivity, materialResource)
+              }]
+            })
+          }
+        }
+      })
+    }
+  })
+
   return listCategoryMaterialResources;
 }
 
-function getMessageCategoryQuantity(categoryResource){
+function getResourceTitle(resourceId) {
+  
+}
+
+function getMessageCategoryQuantity(categoryResourceId){
   var message = "";
 
   return message;
 }
 
-function getMessageWrongCategory(categoryResource){
+function getMessageWrongCategory(categoryResourceId){
   var message = "";
 
   return message;
 }
 
-function getMessageUnavailability(listScheduledActivities, scheduledActivity, resource){
+function getMessageUnavailability(scheduledActivity, resourceId){
   var message = "";
 
   return message;
 }
 
-function getMessageAlreadyExist(listScheduledActivities, scheduledActivity, resource){
+function getMessageAlreadyExist(scheduledActivity, resourceId){
   var message = "";
 
   return message;
 }
 
-function getMessageWorkingHours(scheduledActivity, humanResource){
+function getMessageWorkingHours(scheduledActivity, humanResourceId){
   var message = "";
 
   return message;
 }
+
   function displayListErrorMessages(){
     var lateralPannelBloc=document.querySelectorAll('#'+'lateral-panel-bloc'); 
     var lateralPannel=document.querySelectorAll('#'+'lateral-panel');
