a566e7553b531166daa1fcacbab92fc0ed762891
Schizopinguin <61987113+Schizopinguin@users.noreply.github.com>
2022-07-19 16:30:40 +0200

    delete hover description for modification

--- public/js/planning/modification-planning.js
+++ public/js/planning/modification-planning.js
@@ -883,14 +883,7 @@ function createCalendar(typeResource,useCase) {
       resourceAreaWidth: "20%",
       resourceAreaHeaderContent: headerResources,
 
-      eventDidMount: function (info) {
-        $(info.el).tooltip({
-          title: info.event.extendedProps.description,
-          placement: "top",
-          trigger: "hover",
-          container: "body",
-        });
-      },
+
 
       //permet d'ouvrir la modal pour la modification d'une activité lorsque l'on click dessus
       eventClick: function (event) {
