5c73dd74143e556fd41b0658aa8dd7c05a714fdc
Fourrep <76952150+Fourrep@users.noreply.github.com>
2022-07-29 14:24:57 +0200

    Message erreur plannif standard

--- public/js/planning/modification-planning.js
+++ public/js/planning/modification-planning.js
@@ -1232,7 +1232,7 @@ function getMessageDelay(listScheduledActivities, scheduledActivity) {
   listSuccessors.forEach((successor) => { //browse all successors
     if (successor.idactivitya == scheduledActivity._def.extendedProps.activity) { //if the scheduled activity has a successor
       listScheduledActivities.forEach((scheduledActivityB) => { //browse all events
-        if(scheduledActivityB._def.extendedProps.appointment == scheduledActivity._def.extendedProps.appointment){
+        if (scheduledActivityB._def.extendedProps.appointment == scheduledActivity._def.extendedProps.appointment) {
           if (successor.idactivityb == scheduledActivityB._def.extendedProps.activity) { //if the scheduled activity check is a successor of the scheduled activity verified
             //we check the delay between the two scheduled activities
             var duration = (scheduledActivityB.start.getTime() - scheduledActivity.end.getTime()) / (60 * 1000);
@@ -1763,19 +1763,20 @@ function updatePanelErrorMessages() {
       div.append(divRow);
       var img = document.createElement("img");
       img.src = "/img/exclamation-triangle-fill.svg";
+      img.style.height = "32px";
       var text = document.createElement('h3');
       text.innerHTML = 'Rendez-vous non planifiés';
 
       var expandButton = document.createElement('img');
       expandButton.setAttribute('src', '/img/chevron_down.svg');
       expandButton.setAttribute('id', 'expandButton');
-      expandButton.setAttribute('style', 'background:none;height:50%;position:absolute;right:0%;display:none');
+      expandButton.setAttribute('style', 'background:none;height:50%;position:absolute;right:0%; display:none ;cursor: pointer;');
       expandButton.setAttribute('onclick', "reduceNotification(" + div.id + ")");
 
       var reduceButton = document.createElement('img');
       reduceButton.setAttribute('src', '/img/chevron_up.svg');
       reduceButton.setAttribute('id', 'reduceButton');
-      reduceButton.setAttribute('style', 'background:none;height:50%;position:absolute;right:0%;');
+      reduceButton.setAttribute('style', 'background:none;height:50%;position:absolute;right:0%;cursor: pointer;');
       reduceButton.setAttribute('onclick', "reduceNotification(" + div.id + ")");
       divRow.append(img, text, reduceButton, expandButton);
 
@@ -1801,23 +1802,24 @@ function updatePanelErrorMessages() {
         div.setAttribute('id', 'notification' + i);
         div.setAttribute('style', 'display: flex; flex-direction : column;');
         var divRow = document.createElement('divRow');
-        divRow.setAttribute('style', 'display: flex; flex-direction : row; position : relative');
+        divRow.setAttribute('style', 'display: flex; flex-direction : row; position : relative; justify-content:space-between;');
         div.append(divRow);
         var img = document.createElement("img");
         img.src = "/img/exclamation-triangle-fill.svg";
+        img.style.height = "32px";
         var text = document.createElement('h3');
         text.innerHTML = listErrorMessages.listScheduledAppointment[i].patientName + ' / ' + listErrorMessages.listScheduledAppointment[i].pathwayName;
 
         var reduceButtonAppointment = document.createElement('img');
         reduceButtonAppointment.setAttribute('src', '/img/chevron_up.svg');
         reduceButtonAppointment.setAttribute('id', 'reduceButtonnotification' + i)
-        reduceButtonAppointment.setAttribute('style', 'background:none;height:40%;position:absolute;right:0%;');
+        reduceButtonAppointment.setAttribute('style', 'background:none;height:24px;margin-left:5%;cursor: pointer;');
         reduceButtonAppointment.setAttribute('onclick', "reduceNotification(" + div.id + ")");
 
         var expandButtonAppointment = document.createElement('img');
         expandButtonAppointment.setAttribute('src', '/img/chevron_down.svg');
         expandButtonAppointment.setAttribute('id', 'expandButtonnotification' + i)
-        expandButtonAppointment.setAttribute('style', 'background:none;height:40%;position:absolute;right:0%;display:none');
+        expandButtonAppointment.setAttribute('style', 'background:none;height:24px;display:none;margin-left:5%;cursor: pointer;');
         expandButtonAppointment.setAttribute('onclick', "reduceNotification(" + div.id + ")");
 
         divRow.append(img, text, reduceButtonAppointment, expandButtonAppointment);
