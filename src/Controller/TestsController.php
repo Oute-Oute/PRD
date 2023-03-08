538fd170c8c73f5645df922ade0f86020dbd17c3
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2023-03-08 17:01:28 +0100

    add serialization

--- src/Controller/TestsController.php
+++ src/Controller/TestsController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 
 class TestsController extends AbstractController
@@ -33,6 +36,55 @@ class TestsController extends AbstractController
             'controller_name' => 'TestsController',
             'currentdate' => $date,
         ]);
+    }
+
+    public function serializeDB(ManagerRegistry $doctrine): Response
+    {
+        $entities = [];
+        $filenames = ["patients", "appointments", "humanresources", "categoryofhumanresources", "workinghours", "materialresources", "categoryofmaterialresources", "unavailabilities", "unavailabilityhumanresources", "unavailabilitymaterialresources", "scheduledactivities", "humanresourcescheduled", "materialresourcescheduled"];
+        $entities[0] = $doctrine->getRepository("App\Entity\Patient")->findAll();
+        $entities[1] = $doctrine->getRepository("App\Entity\Appointment")->findAll();
+        $entities[2] = $doctrine->getRepository("App\Entity\HumanResource")->findAll();
+        $entities[3] = $doctrine->getRepository("App\Entity\CategoryOfHumanResource")->findAll();
+        $entities[4] = $doctrine->getRepository("App\Entity\WorkingHours")->findAll();
+        $entities[5] = $doctrine->getRepository("App\Entity\MaterialResource")->findAll();
+        $entities[6] = $doctrine->getRepository("App\Entity\CategoryOfMaterialResource")->findAll();
+        $entities[7] = $doctrine->getRepository("App\Entity\Unavailability")->findAll();
+        $entities[8] = $doctrine->getRepository("App\Entity\UnavailabilityHumanResource")->findAll();
+        $entities[9] = $doctrine->getRepository("App\Entity\UnavailabilityMaterialResource")->findAll();
+        $entities[10] = $doctrine->getRepository("App\Entity\ScheduledActivity")->findAll();
+        $entities[11] = $doctrine->getRepository("App\Entity\HumanResourceScheduled")->findAll();
+        $entities[12] = $doctrine->getRepository("App\Entity\MaterialResourceScheduled")->findAll();
+        //var_dump($doctrine->getRepository("App\Entity\HumanResourceScheduled"));
+        $dir = "Simulations";
+        $newDir = $dir . "/2";
+        if (!file_exists($newDir)) {
+            mkdir($newDir, 0777, true);
+        }
+        for ($i = 0; $i < sizeof($entities); $i++) {
+            $file = fopen($newDir . "/" . $filenames[$i] . ".json", "w");
+            $serializer = new Serializer([new ObjectNormalizer()]);
+            $formatted = $serializer->normalize($entities[$i]);
+            fwrite($file, json_encode($formatted));
+            fclose($file);
+        }
+        //drain the tables
+        $doctrine->getRepository("App\Entity\HumanResourceScheduled")->deleteALl();
+        $doctrine->getRepository("App\Entity\MaterialResourceScheduled")->deleteALl();
+        $doctrine->getRepository("App\Entity\ScheduledActivity")->deleteALl();
+        $doctrine->getRepository("App\Entity\UnavailabilityHumanResource")->deleteALl();
+        $doctrine->getRepository("App\Entity\UnavailabilityMaterialResource")->deleteALl();
+        $doctrine->getRepository("App\Entity\Unavailability")->deleteALl();
+        $doctrine->getRepository("App\Entity\WorkingHours")->deleteALl();
+        $doctrine->getRepository("App\Entity\HumanResource")->deleteALl();
+        $doctrine->getRepository("App\Entity\CategoryOfHumanResource")->deleteALl();
+        $doctrine->getRepository("App\Entity\MaterialResource")->deleteALl();
+        $doctrine->getRepository("App\Entity\CategoryOfMaterialResource")->deleteALl();
+        $doctrine->getRepository("App\Entity\Appointment")->deleteALl();
+        $doctrine->getRepository("App\Entity\Patient")->deleteALl();
+
+
+        return new JsonResponse($formatted);
     }
 
 }
